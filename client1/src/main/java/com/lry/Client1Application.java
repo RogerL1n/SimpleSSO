508f223a67b509cd046e4638f22f478fb92c1696
caiyuwei <2495558580@qq.com>
2023-08-07 01:42:11 +0800

    Initial commit

---
+++ client1/src/main/java/com/lry/Client1Application.java
@@ -0,0 +1,15 @@
+package com.lry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lin
+ */
+@SpringBootApplication
+public class Client1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client1Application.class, args);
+    }
+}
